4723b134e583e12ad112082b0a94ef0a07fd1e42
roche <roche@d09c93e1-fcdf-0473-bb2a-b384122bffe7>
2012-03-24 12:21:42 +0000

    GPL v3

--- trunk/src/htsrobots.c
+++ trunk/src/htsrobots.c
@@ -5,7 +5,7 @@ Copyright (C) Xavier Roche and other contributors
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
+as published by the Free Software Foundation; either version 3
 of the License, or any later version.
 
 This program is distributed in the hope that it will be useful,
