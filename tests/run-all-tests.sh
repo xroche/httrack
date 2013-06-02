6481840d5f6b3129820c9e151c0965d67468199e
Xavier Roche <xroche@users.noreply.github.com>
2013-06-02 17:53:36 +0000

    More checks in tests

--- tests/run-all-tests.sh
+++ tests/run-all-tests.sh
@@ -7,14 +7,14 @@ for i in *.test ; do
 		echo "$i: passed" >&2
 	else
 		echo "$i: ERROR" >&2
-		error=1
+		error=$[${error}+1]
 	fi
 done
 
 if test "$error" -eq 0; then
 	echo "all tests passed" >&2
 else
-	echo "one or more tests failed" >&2
+	echo "${error} test(s) failed" >&2
 fi
 
 exit $error
