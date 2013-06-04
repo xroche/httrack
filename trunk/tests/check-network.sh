247e796b3099b7ecd21320627af31865f4cfdde8
roche <roche@d09c93e1-fcdf-0473-bb2a-b384122bffe7>
2013-06-04 17:18:25 +0000

    Do not execute tests unless external network is available.

---
+++ trunk/tests/check-network.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+
+# ensure the httrack unit tests are available so that ut will not break
+# the build in case of network outage
+
+# test url
+url=http://ut.httrack.com/enabled
+
+# cache file name
+cache=check-network_sh.cache
+
+# cached result ?
+if test -f $cache ; then
+	if grep -q "ok" $cache ; then
+		exit 0
+	else
+		exit 1
+	fi
+fi
+
+# fetch single file
+if bash crawl-test.sh --errors 0 --files 1 --summary httrack --timeout=3 --max-time=3 "$url" 2>/dev/null >/dev/null ; then
+	echo "ok" > $cache
+	exit 0
+else
+	echo "error" > $cache
+	exit 1
+fi
