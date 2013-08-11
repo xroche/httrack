230ee4ded85d30661126de46dd553e841c0f7796
roche <roche@d09c93e1-fcdf-0473-bb2a-b384122bffe7>
2013-08-11 09:21:27 +0000

    Updated remarks

--- android/trunk/jni/Android.mk
+++ android/trunk/jni/Android.mk
@@ -25,15 +25,25 @@ LOCAL_MODULE    := libiconv
 LOCAL_SRC_FILES := ../prebuild/libiconv.so
 include $(PREBUILT_SHARED_LIBRARY)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE    := libcrypto
-LOCAL_SRC_FILES := ../prebuild/libcrypto.so
-include $(PREBUILT_SHARED_LIBRARY)
+# TODO FIXME: INVESTIGATE THIS
+# Android seems to have both /system/lib/libcrypto.so and
+# /system/lib/libssl.so, and Java loads them in priority when using
+# System.loadLibrary(...)
+# We should either ensure that the system libs are fine (they seems fine BTW)
+# or explicitly load the application-cached-library using some dirty magic
+# (ie. giving the explicit full path and .so)
+# For now, we do not ship the libraries, which will spare some space, and
+# hope the ABI won't change too much.
 
-include $(CLEAR_VARS)
-LOCAL_MODULE    := libssl
-LOCAL_SRC_FILES := ../prebuild/libssl.so
-include $(PREBUILT_SHARED_LIBRARY)
+# include $(CLEAR_VARS)
+# LOCAL_MODULE    := libcrypto
+# LOCAL_SRC_FILES := ../prebuild/libcrypto.so
+# include $(PREBUILT_SHARED_LIBRARY)
+
+# include $(CLEAR_VARS)
+# LOCAL_MODULE    := libssl
+# LOCAL_SRC_FILES := ../prebuild/libssl.so
+# include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE    := libhttrack
@@ -50,7 +60,5 @@ LOCAL_MODULE    := htslibjni
 LOCAL_SRC_FILES := htslibjni.c
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../include
 LOCAL_SHARED_LIBRARIES := libhttrack
-#LOCAL_SHARED_LIBRARIES := libhttrack libhtsjava libiconv
-#LOCAL_LDLIBS := -lhttrack
 LOCAL_CFLAGS := -W -Wall -Wextra -Werror
 include $(BUILD_SHARED_LIBRARY)
