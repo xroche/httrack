ec80d9a35edc279da3bde9b652b043de359a5233
roche <roche@d09c93e1-fcdf-0473-bb2a-b384122bffe7>
2013-09-13 19:29:17 +0000

    Merged revisions 851,853,855-857,861-862 via svnmerge from http://proliant/svn/httrack/trunk
    ........
      r851 | roche | 2013-09-09 19:20:34 +0200 (lun., 09 sept. 2013) | 3 lines
      
      Fixed Keywords field in desktop files (Sebastian Pipping)
    ........
      r853 | roche | 2013-09-13 17:54:23 +0200 (ven., 13 sept. 2013) | 4 lines
      
      Fixed FSF GPL reference (postal address removed, added website)
      Fixed year notice
    ........
      r855 | roche | 2013-09-13 18:08:40 +0200 (ven., 13 sept. 2013) | 3 lines
      
      AUTHORS should be 644
    ........
      r856 | roche | 2013-09-13 18:12:52 +0200 (ven., 13 sept. 2013) | 3 lines
      
      Replaced AM_PROG_LIBTOOL by LT_INIT
    ........
      r857 | roche | 2013-09-13 18:13:02 +0200 (ven., 13 sept. 2013) | 3 lines
      
      Updated man
    ........
      r861 | roche | 2013-09-13 21:21:03 +0200 (ven., 13 sept. 2013) | 3 lines
      
      Fixed license and copyright
    ........
      r862 | roche | 2013-09-13 21:26:08 +0200 (ven., 13 sept. 2013) | 3 lines
      
      Fixed man
    ........

--- branches/3.47/src/htsname.c
+++ branches/3.47/src/htsname.c
@@ -1,12 +1,12 @@
 /* ------------------------------------------------------------ */
 /*
 HTTrack Website Copier, Offline Browser for Windows and Unix
-Copyright (C) Xavier Roche and other contributors
+Copyright (C) 1998-2013 Xavier Roche and other contributors
 
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 3
-of the License, or any later version.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -14,8 +14,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 Important notes:
 
