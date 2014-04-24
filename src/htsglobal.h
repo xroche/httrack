75b5002c63ea70afcd2cf7b7dc3634d60c589745
Xavier Roche <xroche@users.noreply.github.com>
2014-04-24 19:32:26 +0000

    More 2014.

--- src/htsglobal.h
+++ src/htsglobal.h
@@ -206,7 +206,7 @@ Please visit our Website: http://www.httrack.com
 #endif
 
 /* Copyright (C) 1998-2014 Xavier Roche and other contributors */
-#define HTTRACK_AFF_AUTHORS "[XR&CO'2013]"
+#define HTTRACK_AFF_AUTHORS "[XR&CO'2014]"
 #define HTS_DEFAULT_FOOTER "<!-- Mirrored from %s%s by HTTrack Website Copier/"HTTRACK_AFF_VERSION" "HTTRACK_AFF_AUTHORS", %s -->"
 #define HTTRACK_WEB "http://www.httrack.com"
 #define HTS_UPDATE_WEBSITE "http://www.httrack.com/update.php3?Product=HTTrack&Version="HTTRACK_VERSIONID"&VersionStr="HTTRACK_VERSION"&Platform=%d&Language=%s"
