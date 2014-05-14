9b66ae12b8c3ce457a32135c3dfb56b030effb9d
roche <roche@d09c93e1-fcdf-0473-bb2a-b384122bffe7>
2014-05-14 18:42:23 +0000

    3.48.7 tag

---
+++ tags/3.48.7/libtest/callbacks-example-listlinks.c
@@ -0,0 +1,149 @@
+/*
+    HTTrack external callbacks example
+    .c file
+
+    How to build: (callback.so or callback.dll)
+      With GNU-GCC:
+        gcc -O -g3 -Wall -D_REENTRANT -shared -o mycallback.so callbacks-example.c -lhttrack2
+      With MS-Visual C++:
+        cl -LD -nologo -W3 -Zi -Zp4 -DWIN32 -Fe"mycallback.dll" callbacks-example.c libhttrack.lib
+
+      Note: the httrack library linker option is only necessary when using libhttrack's functions inside the callback
+
+    How to use:
+      httrack --wrapper mycallback ..
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* Standard httrack module includes */
+#include "httrack-library.h"
+#include "htsopt.h"
+#include "htsdefines.h"
+
+/* Function definitions */
+static int process_file(t_hts_callbackarg * carg, httrackp * opt, char *html,
+                        int len, const char *url_address, const char *url_file);
+static int check_detectedlink(t_hts_callbackarg * carg, httrackp * opt,
+                              char *link);
+static int check_loop(t_hts_callbackarg * carg, httrackp * opt, lien_back *back,
+                      int back_max, int back_index, int lien_tot, int lien_ntot,
+                      int stat_time, hts_stat_struct * stats);
+static int end(t_hts_callbackarg * carg, httrackp * opt);
+
+/* external functions */
+EXTERNAL_FUNCTION int hts_plug(httrackp * opt, const char *argv);
+
+/*
+  This sample just lists all links in documents with the parent link:
+  <parent> -> <link>
+  This sample can be improved, for example, to make a map of a website.
+*/
+
+typedef struct t_my_userdef {
+  char currentURLBeingParsed[2048];
+} t_my_userdef;
+
+/* 
+module entry point 
+*/
+EXTERNAL_FUNCTION int hts_plug(httrackp * opt, const char *argv) {
+  t_my_userdef *userdef;
+
+  /* */
+  const char *arg = strchr(argv, ',');
+
+  if (arg != NULL)
+    arg++;
+
+  /* Create user-defined structure */
+  userdef = (t_my_userdef *) malloc(sizeof(t_my_userdef));      /* userdef */
+  userdef->currentURLBeingParsed[0] = '\0';
+
+  /* Plug callback functions */
+  CHAIN_FUNCTION(opt, check_html, process_file, userdef);
+  CHAIN_FUNCTION(opt, end, end, userdef);
+  CHAIN_FUNCTION(opt, linkdetected, check_detectedlink, userdef);
+  CHAIN_FUNCTION(opt, loop, check_loop, userdef);
+
+  return 1;                     /* success */
+}
+
+static int process_file(t_hts_callbackarg * carg, httrackp * opt, char *html,
+                        int len, const char *url_address,
+                        const char *url_file) {
+  t_my_userdef *userdef = (t_my_userdef *) CALLBACKARG_USERDEF(carg);
+  char *const currentURLBeingParsed = userdef->currentURLBeingParsed;
+
+  /* Call parent functions if multiple callbacks are chained. */
+  if (CALLBACKARG_PREV_FUN(carg, check_html) != NULL) {
+    if (!CALLBACKARG_PREV_FUN(carg, check_html)
+        (CALLBACKARG_PREV_CARG(carg), opt, html, len, url_address, url_file)) {
+      return 0;                 /* Abort */
+    }
+  }
+
+  /* Process */
+  printf("now parsing %s%s..\n", url_address, url_file);
+  strcpy(currentURLBeingParsed, url_address);
+  strcat(currentURLBeingParsed, url_file);
+
+  return 1;                     /* success */
+}
+
+static int check_detectedlink(t_hts_callbackarg * carg, httrackp * opt,
+                              char *link) {
+  t_my_userdef *userdef = (t_my_userdef *) CALLBACKARG_USERDEF(carg);
+  char *const currentURLBeingParsed = userdef->currentURLBeingParsed;
+
+  /* Call parent functions if multiple callbacks are chained. */
+  if (CALLBACKARG_PREV_FUN(carg, linkdetected) != NULL) {
+    if (!CALLBACKARG_PREV_FUN(carg, linkdetected)
+        (CALLBACKARG_PREV_CARG(carg), opt, link)) {
+      return 0;                 /* Abort */
+    }
+  }
+
+  /* Process */
+  printf("[%s] -> [%s]\n", currentURLBeingParsed, link);
+
+  return 1;                     /* success */
+}
+
+static int check_loop(t_hts_callbackarg * carg, httrackp * opt, lien_back *back,
+                      int back_max, int back_index, int lien_tot, int lien_ntot,
+                      int stat_time, hts_stat_struct * stats) {
+  static int fun_animation = 0;
+
+  /* Call parent functions if multiple callbacks are chained. */
+  if (CALLBACKARG_PREV_FUN(carg, loop) != NULL) {
+    if (!CALLBACKARG_PREV_FUN(carg, loop)
+        (CALLBACKARG_PREV_CARG(carg), opt, back, back_max, back_index, lien_tot,
+         lien_ntot, stat_time, stats)) {
+      return 0;                 /* Abort */
+    }
+  }
+
+  /* Process */
+  printf("%c\r", "/-\\|"[(fun_animation++) % 4]);
+  return 1;
+}
+
+static int end(t_hts_callbackarg * carg, httrackp * opt) {
+  t_my_userdef *userdef = (t_my_userdef *) CALLBACKARG_USERDEF(carg);
+
+  fprintf(stderr, "** info: wrapper_exit() called!\n");
+  if (userdef != NULL) {
+    free(userdef);
+    userdef = NULL;
+  }
+
+  /* Call parent functions if multiple callbacks are chained. */
+  if (CALLBACKARG_PREV_FUN(carg, end) != NULL) {
+    return CALLBACKARG_PREV_FUN(carg, end) (CALLBACKARG_PREV_CARG(carg), opt);
+  }
+
+  return 1;                     /* success */
+}
---
+++ tags/3.48.7/src/htshelp.h
@@ -0,0 +1,55 @@
+/* ------------------------------------------------------------ */
+/*
+HTTrack Website Copier, Offline Browser for Windows and Unix
+Copyright (C) 1998-2014 Xavier Roche and other contributors
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+Important notes:
+
+- We hereby ask people using this source NOT to use it in purpose of grabbing
+emails addresses, or collecting any other private information on persons.
+This would disgrace our work, and spoil the many hours we spent on it.
+
+Please visit our Website: http://www.httrack.com
+*/
+
+/* ------------------------------------------------------------ */
+/* File: httrack.c subroutines:                                 */
+/*       command-line help system                               */
+/* Author: Xavier Roche                                         */
+/* ------------------------------------------------------------ */
+
+#ifndef HTSHELP_DEFH
+#define HTSHELP_DEFH
+
+/* Library internal definictions */
+#ifdef HTS_INTERNAL_BYTECODE
+
+/* Forward definitions */
+#ifndef HTS_DEF_FWSTRUCT_httrackp
+#define HTS_DEF_FWSTRUCT_httrackp
+typedef struct httrackp httrackp;
+#endif
+
+void infomsg(char *msg);
+void help(char *app, int more);
+void make_empty_index(char *str);
+void help_wizard(httrackp * opt);
+int help_query(char *list, int def);
+void help_catchurl(const char *dest_path);
+
+#endif
+
+#endif
