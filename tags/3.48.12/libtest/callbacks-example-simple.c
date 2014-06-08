2311f35874fcd9da739162743553b72bac57830a
roche <roche@d09c93e1-fcdf-0473-bb2a-b384122bffe7>
2014-06-08 16:16:13 +0000

    created tag 3.48.12

---
+++ tags/3.48.12/libtest/callbacks-example-simple.c
@@ -0,0 +1,100 @@
+/*
+    HTTrack external callbacks example : print all downloaded html documents
+
+    How to build: (callback.so or callback.dll)
+      With GNU-GCC:
+        gcc -O -g3 -Wall -D_REENTRANT -shared -o mycallback.so callbacks-example.c -lhttrack2
+      With MS-Visual C++:
+        cl -LD -nologo -W3 -Zi -Zp4 -DWIN32 -Fe"mycallback.dll" callbacks-example.c libhttrack.lib
+
+      Note: the httrack library linker option is only necessary when using libhttrack's functions inside the callback
+
+    How to use:
+      httrack --wrapper mycallback ..
+*/
+
+/* system includes */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* standard httrack module includes */
+#include "httrack-library.h"
+#include "htsopt.h"
+#include "htsdefines.h"
+
+/* external functions */
+EXTERNAL_FUNCTION int hts_plug(httrackp * opt, const char *argv);
+EXTERNAL_FUNCTION int hts_unplug(httrackp * opt);
+
+/* local function called as "check_html" callback */
+static int process_file(t_hts_callbackarg
+                        /*the carg structure, holding various information */  *
+                        carg, /*the option settings */ httrackp * opt,
+                        /*other parameters are callback-specific */
+                        char *html, int len, const char *url_address,
+                        const char *url_file) {
+  void *ourDummyArg = (void *) CALLBACKARG_USERDEF(carg);       /*optional user-defined arg */
+
+  (void) ourDummyArg;
+
+  /* call parent functions if multiple callbacks are chained. you can skip this part, if you don't want previous callbacks to be called. */
+  if (CALLBACKARG_PREV_FUN(carg, check_html) != NULL) {
+    if (!CALLBACKARG_PREV_FUN(carg, check_html)
+        (CALLBACKARG_PREV_CARG(carg), opt, html, len, url_address, url_file)) {
+      return 0;                 /* abort */
+    }
+  }
+
+  printf("file %s%s content: %s\n", url_address, url_file, html);
+  return 1;                     /* success */
+}
+
+/* local function called as "end" callback */
+static int end_of_mirror(t_hts_callbackarg
+                         /*the carg structure, holding various information */  *
+                         carg, /*the option settings */ httrackp * opt) {
+  void *ourDummyArg = (void *) CALLBACKARG_USERDEF(carg);       /*optional user-defined arg */
+
+  (void) ourDummyArg;
+
+  /* processing */
+  fprintf(stderr, "That's all, folks!\n");
+
+  /* call parent functions if multiple callbacks are chained. you can skip this part, if you don't want previous callbacks to be called. */
+  if (CALLBACKARG_PREV_FUN(carg, end) != NULL) {
+    /* status is ok on our side, return other callabck's status */
+    return CALLBACKARG_PREV_FUN(carg, end) (CALLBACKARG_PREV_CARG(carg), opt);
+  }
+
+  return 1;                     /* success */
+}
+
+/*
+module entry point
+the function name and prototype MUST match this prototype
+*/
+EXTERNAL_FUNCTION int hts_plug(httrackp * opt, const char *argv) {
+  /* optional argument passed in the commandline we won't be using here */
+  const char *arg = strchr(argv, ',');
+
+  if (arg != NULL)
+    arg++;
+
+  /* plug callback functions */
+  CHAIN_FUNCTION(opt, check_html, process_file, /*optional user-defined arg */
+                 NULL);
+  CHAIN_FUNCTION(opt, end, end_of_mirror, /*optional user-defined arg */ NULL);
+
+  return 1;                     /* success */
+}
+
+/*
+module exit point
+the function name and prototype MUST match this prototype
+*/
+EXTERNAL_FUNCTION int hts_unplug(httrackp * opt) {
+  fprintf(stderr, "Module unplugged");
+
+  return 1;                     /* success */
+}
---
+++ tags/3.48.12/src/htsrobots.c
@@ -0,0 +1,116 @@
+/* ------------------------------------------------------------ */
+/*
+HTTrack Website Copier, Offline Browser for Windows and Unix
+Copyright (C) 1998-2014 Xavier Roche and other contributors
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+Important notes:
+
+- We hereby ask people using this source NOT to use it in purpose of grabbing
+emails addresses, or collecting any other private information on persons.
+This would disgrace our work, and spoil the many hours we spent on it.
+
+Please visit our Website: http://www.httrack.com
+*/
+
+/* ------------------------------------------------------------ */
+/* File: httrack.c subroutines:                                 */
+/*       robots.txt (website robot file)                        */
+/* Author: Xavier Roche                                         */
+/* ------------------------------------------------------------ */
+
+/* Internal engine bytecode */
+#define HTS_INTERNAL_BYTECODE
+
+/* specific definitions */
+#include "htscore.h"
+#include "htsbase.h"
+#include "htslib.h"
+/* END specific definitions */
+
+#include "htsrobots.h"
+
+// -- robots --
+
+// fil="" : vérifier si règle déja enregistrée
+int checkrobots(robots_wizard * robots, const char *adr, const char *fil) {
+  while(robots) {
+    if (strfield2(robots->adr, adr)) {
+      if (fil[0]) {
+        int ptr = 0;
+        char line[250];
+
+        if (strnotempty(robots->token)) {
+          do {
+            ptr += binput(robots->token + ptr, line, 200);
+            if (line[0] == '/') {       // absolu
+              if (strfield(fil, line)) {        // commence avec ligne
+                return -1;      // interdit
+              }
+            } else {            // relatif
+              if (strstrcase((char*) fil, line)) {
+                return -1;
+              }
+            }
+          } while((strnotempty(line)) && (ptr < (int) strlen(robots->token)));
+        }
+      } else {
+        return -1;
+      }
+    }
+    robots = robots->next;
+  }
+  return 0;
+}
+int checkrobots_set(robots_wizard * robots, const char *adr, const char *data) {
+  if (((int) strlen(adr)) >= sizeof(robots->adr) - 2)
+    return 0;
+  if (((int) strlen(data)) >= sizeof(robots->token) - 2)
+    return 0;
+  while(robots) {
+    if (strfield2(robots->adr, adr)) {  // entrée existe
+      strcpybuff(robots->token, data);
+#if DEBUG_ROBOTS
+      printf("robots.txt: set %s to %s\n", adr, data);
+#endif
+      return -1;
+    } else if (!robots->next) {
+      robots->next = (robots_wizard *) calloct(1, sizeof(robots_wizard));
+      if (robots->next) {
+        robots->next->next = NULL;
+        strcpybuff(robots->next->adr, adr);
+        strcpybuff(robots->next->token, data);
+#if DEBUG_ROBOTS
+        printf("robots.txt: new set %s to %s\n", adr, data);
+#endif
+      }
+#if DEBUG_ROBOTS
+      else
+        printf("malloc error!!\n");
+#endif
+    }
+    robots = robots->next;
+  }
+  return 0;
+}
+void checkrobots_free(robots_wizard * robots) {
+  if (robots->next) {
+    checkrobots_free(robots->next);
+    freet(robots->next);
+    robots->next = NULL;
+  }
+}
+
+// -- robots --
