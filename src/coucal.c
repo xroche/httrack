a431fcf71728967999e6a249d90f43353fb44c19
Xavier Roche <xroche@users.noreply.github.com>
2014-06-23 07:33:30 +0000

    #ifdef failed

--- src/coucal.c
+++ src/coucal.c
@@ -59,7 +59,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 #if (!defined(HTS_INTHASH_USES_MD5) && !defined(HTS_INTHASH_USES_MURMUR))
 /* Temporry: fixing Invalid address alignment issues */
-#ifdef defined(__ANDROID__)
+#if defined(__ANDROID__)
 #define HTS_INTHASH_USES_MD5 1
 #else
 #define HTS_INTHASH_USES_MURMUR 1
