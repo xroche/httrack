b7a55e7ff8d7c2054d9a2f088cb9ae4bb3f76cde
roche <roche@d09c93e1-fcdf-0473-bb2a-b384122bffe7>
2014-10-15 19:45:19 +0000

    Merged revisions 1288,1291-1293,1296-1297,1303,1305-1307,1311,1313-1315 via svnmerge from http://proliant/svn/httrack/trunk
    ........
      r1288 | roche | 2014-07-20 11:19:50 +0200 (dim., 20 juil. 2014) | 3 lines
      
      Additional message when crashing to report the issue.
    ........
      r1291 | roche | 2014-07-28 22:56:38 +0200 (lun., 28 juil. 2014) | 4 lines
      
      Fixed 'strlen(copyBuff) == qLen failed at htslib.c:3458' assertion failure.
      Rationale: strncat(..., ..., (size_t) -1) does not behave gently on Linux, and is not equivalent to strcat(..., ...) when using optimizations (could it be a corner-case bug ?)
    ........
      r1292 | roche | 2014-07-28 22:56:57 +0200 (lun., 28 juil. 2014) | 3 lines
      
      Added testcase for 'strlen(copyBuff) == qLen failed at htslib.c:3458'
    ........
      r1293 | roche | 2014-07-28 23:02:00 +0200 (lun., 28 juil. 2014) | 3 lines
      
      3.48.19
    ........
      r1296 | roche | 2014-07-28 23:16:44 +0200 (lun., 28 juil. 2014) | 3 lines
      
      Reverted useless check.
    ........
      r1297 | roche | 2014-07-28 23:19:57 +0200 (lun., 28 juil. 2014) | 3 lines
      
      Updated date
    ........
      r1303 | roche | 2014-08-16 10:16:08 +0200 (sam., 16 août 2014) | 3 lines
      
      Fixed "configure script cannot detect libressl" (junk4me46806)
    ........
      r1305 | roche | 2014-08-27 19:01:26 +0200 (mer., 27 août 2014) | 4 lines
      
      Do not include execinfo.h if missing (Android)
      Do not use strnlen if missing (Mac OS X 10.6.8) ( nthpowers)
    ........
      r1306 | roche | 2014-08-27 20:10:28 +0200 (mer., 27 août 2014) | 3 lines
      
      Off-by-one.
    ........
      r1307 | roche | 2014-08-27 20:22:56 +0200 (mer., 27 août 2014) | 3 lines
      
      Fixed HAVE_STRNLEN (sheesh, we don't have strnlen.o)
    ........
      r1311 | roche | 2014-10-04 09:39:18 +0200 (sam., 04 oct. 2014) | 1 line
      
      Fixed typo
    ........
      r1313 | roche | 2014-10-15 19:33:35 +0200 (mer., 15 oct. 2014) | 3 lines
      
      Typo :)
    ........
      r1314 | roche | 2014-10-15 21:17:29 +0200 (mer., 15 oct. 2014) | 8 lines
      
      Fixed webhttrack incompatibility with Chrome
        * closes:#53
      
      Also fixed HTML-escaping issues inside webhttrack
      
      Rationale: The webhttrack script made the wrong assumption that once the "browse" command returned, it meant the user killed the navigation window, and it had to kill the server itself. However, modern browsers tend to "attach" to an existing session (creating a new tab, for example, within an existing window), causing the browsing command to return immediately, thus causing the server to be killed immediately by the webhttrack script. I have rewritten the logic behind, and now the server is able to kill himself if the parent script dies, AND if the browsing client did not make any activity for two minutes. The "activity" can be any browser/refreshed page, or the internal "ping" iframe (which pings the server every 30 seconds). With this model, we *should* be compatible with old browsers, and modern ones.
    ........
      r1315 | roche | 2014-10-15 21:44:22 +0200 (mer., 15 oct. 2014) | 3 lines
      
      3.48.20
    ........

--- branches/3.48/src/htssafe.h
+++ branches/3.48/src/htssafe.h
@@ -101,7 +101,7 @@ static HTS_UNUSED void abortf_(const char *exp, const char *file, int line) {
 }
 
 /**
- * Check wether 'VAR' is of type char[].
+ * Check whether 'VAR' is of type char[].
  */
 #if (defined(__GNUC__) && !defined(__cplusplus))
 /* Note: char[] and const char[] are compatible */
@@ -136,6 +136,18 @@ static HTS_UNUSED void htssafe_compile_time_check_(void) {
   "overflow while appending '" #B "' to '"#A"'", __FILE__, __LINE__) )
 
 /**
+ * Append characters of "B" to "A".
+ * If "A" is a char[] variable whose size is not sizeof(char*), then the size 
+ * is assumed to be the capacity of this array.
+ */
+#define strcatbuff(A, B) \
+  ( HTS_IS_NOT_CHAR_BUFFER(A) \
+  ? strcat(A, B) \
+  : strncat_safe_(A, sizeof(A), B, \
+  HTS_IS_NOT_CHAR_BUFFER(B) ? (size_t) -1 : sizeof(B), (size_t) -1, \
+  "overflow while appending '" #B "' to '"#A"'", __FILE__, __LINE__) )
+
+/**
  * Copy characters from "B" to "A".
  * If "A" is a char[] variable whose size is not sizeof(char*), then the size 
  * is assumed to be the capacity of this array.
@@ -146,15 +158,6 @@ static HTS_UNUSED void htssafe_compile_time_check_(void) {
   : strcpy_safe_(A, sizeof(A), B, \
   HTS_IS_NOT_CHAR_BUFFER(B) ? (size_t) -1 : sizeof(B), \
   "overflow while copying '" #B "' to '"#A"'", __FILE__, __LINE__) )
-
-/* note: "size_t is an unsigned integral type" */
-
-/**
- * Append characters of "B" to "A".
- * If "A" is a char[] variable whose size is not sizeof(char*), then the size 
- * is assumed to be the capacity of this array.
- */
-#define strcatbuff(A, B) strncatbuff(A, B, (size_t) -1)
 
 /**
  * Append characters of "B" to "A", "A" having a maximum capacity of "S".
@@ -172,6 +175,15 @@ static HTS_UNUSED void htssafe_compile_time_check_(void) {
   HTS_IS_NOT_CHAR_BUFFER(B) ? (size_t) -1 : sizeof(B), \
   "overflow while copying '" #B "' to '"#A"'", __FILE__, __LINE__)
 
+/** strnlen replacement (autotools). **/
+#if ( ! defined(_WIN32) && ! defined(HAVE_STRNLEN) )
+static HTS_UNUSED size_t strnlen(const char *s, size_t maxlen) {
+  size_t i;
+  for(i = 0 ; i < maxlen && s[i] != '\0' ; i++) ;
+  return i;
+}
+#endif
+
 static HTS_INLINE HTS_UNUSED size_t strlen_safe_(const char *source, const size_t sizeof_source, 
                                                  const char *file, int line) {
   size_t size;
@@ -188,6 +200,7 @@ static HTS_INLINE HTS_UNUSED char* strncat_safe_(char *const dest, const size_t 
                                                  const char *exp, const char *file, int line) {
   const size_t source_len = strlen_safe_(source, sizeof_source, file, line);
   const size_t dest_len = strlen_safe_(dest, sizeof_dest, file, line);
+  /* note: "size_t is an unsigned integral type" ((size_t) -1 is positive) */
   const size_t source_copy = source_len <= n ? source_len : n;
   const size_t dest_final_len = dest_len + source_copy;
   assertf__(dest_final_len < sizeof_dest, exp, file, line);
